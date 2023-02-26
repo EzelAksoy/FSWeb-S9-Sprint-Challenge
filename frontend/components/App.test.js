7cd3789b1a2faec9d6dede10f827fefb98e38813
EzelAksoy <aksoy.ezel@gmail.com>
2023-02-27 00:29:38 +0300

    ödev tamamlandı.

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,42 @@
 // Write your tests here
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import AppFunctional from "./AppFunctional";
+
+test("Sol Test", async () => {
+  render(<AppFunctional />);
+  fireEvent.click(screen.getByTestId("sol-button"));
+  fireEvent.click(screen.getByTestId("sol-button"));
+  expect(screen.getByText("Sola Gidemezsiniz!")).toBeInTheDocument();
+});
+
+test("Sag test", async () => {
+  render(<AppFunctional />);
+  fireEvent.click(screen.getByTestId("sağ-button"));
+  fireEvent.click(screen.getByTestId("sağ-button"));
+  expect(screen.getByText("Sağa Gidemezsiniz!")).toBeInTheDocument();
+});
+
+test("Yukari test", async () => {
+  render(<AppFunctional />);
+  fireEvent.click(screen.getByTestId("yukari-button"));
+  fireEvent.click(screen.getByTestId("yukari-button"));
+  expect(screen.getByText("Yukarıya Gidemezsiniz!")).toBeInTheDocument();
+});
+
+test("Asagi test", async () => {
+  render(<AppFunctional />);
+  fireEvent.click(screen.getByTestId("aşaği-button"));
+  fireEvent.click(screen.getByTestId("aşaği-button"));
+  expect(screen.getByText("Aşağıya Gidemezsiniz!")).toBeInTheDocument();
+});
+test("reset test", async () => {
+  render(<AppFunctional />);
+  let komutlar = ["yukari", "aşaği", "aşaği", "sol", "sağ", "sağ", "reset"];
+  komutlar.forEach((komut) => {
+    fireEvent.click(screen.getByTestId(`${komut}-button`));
+  });
+  expect(screen.getByText("Koordinatlar (2, 2)")).toBeInTheDocument();
+  expect(screen.getByText("0 kere ilerlediniz")).toBeInTheDocument();
+});
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,170 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 // önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
-
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
+const dummyData = {
+  message: "",
+  email: "",
+  steps: 0,
+  index: 4,
+};
 export default function AppFunctional(props) {
   // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
   // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+  const [data, setData] = useState(dummyData);
 
   function getXY() {
     // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
     // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+    return { x: (data.index % 3) + 1, y: Math.floor(data.index / 3) + 1 };
   }
 
   function getXYMesaj() {
     // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
     // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
+    // tamamen oluşturulmuş stringi döndürür.,
+    return `Koordinatlar (${getXY().x}, ${getXY().y})`;
   }
 
   function reset() {
     // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setData(dummyData);
   }
 
   function sonrakiIndex(yon) {
     // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
     // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
     // şu anki indeksi değiştirmemeli.
+    let nextIndeks = data.index;
+    if (yon === "left") {
+      if (data.index % 3 !== 0) {
+        nextIndeks = data.index - 1;
+      } else {
+        setData({ ...data, message: "Sola Gidemezsiniz!" });
+      }
+    }
+    if (yon === "up") {
+      if (data.index / 3 >= 1) {
+        nextIndeks = data.index - 3;
+      } else {
+        setData({ ...data, message: "Yukarıya Gidemezsiniz!" });
+      }
+    }
+    if (yon === "right") {
+      if (data.index % 3 < 2) {
+        nextIndeks = data.index + 1;
+      } else {
+        setData({ ...data, message: "Sağa Gidemezsiniz!" });
+      }
+    }
+    if (yon === "down") {
+      if (data.index / 3 < 2) {
+        nextIndeks = data.index + 3;
+      } else {
+        setData({ ...data, message: "Aşağıya Gidemezsiniz!" });
+      }
+    }
+    return nextIndeks;
   }
 
   function ilerle(evt) {
     // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
     // ve buna göre state i değiştirir.
+    let newState = sonrakiIndex(evt.target.id);
+    if (newState !== data.index) {
+      setData({ ...data, message: "", steps: data.steps + 1, index: newState });
+    }
   }
 
   function onChange(evt) {
+    if (evt.target.id === "reset") {
+      reset();
+    } else if (evt.target.id === "email") {
+      setData({ ...data, email: evt.target.value });
+    } else {
+      ilerle(evt);
+    }
     // inputun değerini güncellemek için bunu kullanabilirsiniz
   }
-
+  console.log(data);
   function onSubmit(evt) {
+    evt.preventDefault();
+    let sentIt = {
+      x: getXY().x,
+      y: getXY().y,
+      steps: data.steps,
+      email: data.email,
+    };
+    if (data.email === "") {
+      setData({ ...data, ["message"]: "Geçerli bir e-mail girin lütfen!" });
+    } else {
+      axios
+        .post("http://localhost:9000/api/result", sentIt)
+        .then((result) => {
+          setData({
+            ...data,
+            ["email"]: "",
+            ["message"]: result.data.message,
+          });
+        })
+        .catch((err) =>
+          setData({ ...data, ["message"]: err.response.data.message })
+        );
+    }
     // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
   }
-
+  const sayılar = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">{getXYMesaj()}</h3>
+        <h3 id="steps">{data.steps} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div
+            key={idx}
+            className={`square${idx === data.index ? " active" : ""}`}
+          >
+            {idx === data.index ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{data.message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={onChange} data-testid="sol-button">
+          SOL
+        </button>
+        <button id="up" onClick={onChange} data-testid="yukari-button">
+          YUKARI
+        </button>
+        <button id="right" onClick={onChange} data-testid="sağ-button">
+          SAĞ
+        </button>
+        <button id="down" onClick={onChange} data-testid="aşaği-button">
+          AŞAĞI
+        </button>
+        <button id="reset" onClick={onChange} data-testid="reset-button">
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          onChange={onChange}
+          value={data.email}
+        ></input>
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
